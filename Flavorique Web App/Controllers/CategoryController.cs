53c65d8756e48ab3ef781cd275a7cd6e90987a35
DionKelmendi <108992835+DionKelmendi@users.noreply.github.com>
2023-11-15 09:28:08 +0100

    Added bootstrap theme, Added Base CRUD Views, Updated GitIgnore

--- Flavorique Web App/Controllers/CategoryController.cs
+++ Flavorique Web App/Controllers/CategoryController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Flavorique_Web_App.Models;
 using Flavorique_Web_App.Data;
 
@@ -15,8 +15,112 @@ namespace Flavorique_Web_App.Controllers
 
         public IActionResult Index()
         {
-            IEnumerable<Category> objCategoryList = _db.Categories;
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name.Trim() == obj.DisplayOrder.ToString()) {
+                ModelState.AddModelError("Name", "Name cannot be the same as Display Order.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            // Returns first object from sequence
+            // var categoryFromDbFirst = _db.Categories.FirstOrDefault(c => c.Id == id); 
+
+            // Returns object from sequence where the condition is satisfied, returns exception if more than one object is found
+            // var categoryFromDbSingle = _db.Categories.SingleOrDefault(c => c.Id == id); 
+
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name.Trim() == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Name cannot be the same as Display Order.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+              return NotFound();
+            }
+
+            var obj = _db.Categories.Find(id);
+
+                      if (obj == null)
+                    {
+                      return NotFound();
+                }
+            //  return View(categoryFromDb);
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        //POST
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult DeletePOST(int? id)
+        //{
+          //  var obj = _db.Categories.Find(id);
+            
+            //if (obj == null) {
+              //  return NotFound();
+            //}
+
+            //_db.Categories.Remove(obj);
+            //_db.SaveChanges();
+            //return RedirectToAction("Index");
+       // }
     }
 }
--- Flavorique Web App/Models/Category.cs
+++ Flavorique Web App/Models/Category.cs
@@ -1,17 +1,18 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Configuration;
 
 namespace Flavorique_Web_App.Models
 {
-    
     public class Category
     {
         [Key]
         public int Id { get; set; }
         [Required]
-        public string Name { get; set; }
-        public string DisplayOrder { get; set; }
+        public string? Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100)]
+        public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
-    
     }
 }
