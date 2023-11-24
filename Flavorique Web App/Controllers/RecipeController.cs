206d21b53c615a4d2a5a5410376d19f1693f9b83
DionKelmendi <108992835+DionKelmendi@users.noreply.github.com>
2023-11-24 20:35:44 +0100

    Added CKEditor 5 Support for Recipe Creation

---
+++ Flavorique Web App/Controllers/RecipeController.cs
@@ -0,0 +1,89 @@
+﻿using Flavorique_Web_App.Data;
+using Flavorique_Web_App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flavorique_Web_App.Controllers
+{
+    public class RecipeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public RecipeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Recipe> obj = _db.Recipes.ToList();
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Recipe obj)
+        {
+            return View();
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Recipes.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Recipe obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Recipes.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // POST
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Recipes.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Recipes.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}
--- Flavorique Web App/Program.cs
+++ Flavorique Web App/Program.cs
@@ -1,6 +1,9 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 using Flavorique_Web_App.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,6 +31,41 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// Configure CBox JWT
+app.MapGet("/token", () =>
+{
+
+    var environmentId = builder.Configuration.GetValue<string>("CKBoxEnvironmentId");
+    var accessKey = builder.Configuration.GetValue<string>("CKBoxAccessKey");
+    var securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(accessKey));
+
+    var signingCredentials = new SigningCredentials(securityKey, "HS256");
+    var header = new JwtHeader(signingCredentials);
+
+    var dateTimeOffset = new DateTimeOffset(DateTime.UtcNow);
+
+    var payload = new JwtPayload
+    {
+        { "aud", environmentId },
+        { "iat", dateTimeOffset.ToUnixTimeSeconds() },
+        { "sub", "user-123" },
+        { "user", new Dictionary<string, string> {
+            { "email", "joe.doe@example.com" },
+            { "name", "Joe Doe" }
+        } },
+        { "auth", new Dictionary<string, object> {
+            { "ckbox", new Dictionary<string, string> {
+                { "role", "admin" }
+            } }
+        } }
+    };
+
+    var securityToken = new JwtSecurityToken(header, payload);
+    var handler = new JwtSecurityTokenHandler();
+
+    return handler.WriteToken(securityToken);
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
