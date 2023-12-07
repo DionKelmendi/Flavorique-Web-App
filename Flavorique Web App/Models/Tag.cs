ae2d687210e94e32fb3ecfeff300d0701cc89af9
Anesazhegrova <120370272+Anesazhegrova@users.noreply.github.com>
2023-12-08 00:10:44 +0100

    Created Tag Controller & View: Tag-Index

--- Flavorique Web App/Models/Tag.cs
+++ Flavorique Web App/Models/Tag.cs
@@ -1,8 +1,13 @@
-﻿namespace Flavorique_Web_App.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+namespace Flavorique_Web_App.Models
 {
     public class Tag
     {
+        [Key]
         public int Id {  get; set; }
+        [Required]
         public string Name { get; set; }
     }
 }
