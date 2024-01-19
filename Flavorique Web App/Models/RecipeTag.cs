fae3d89030da5ab7c2aff4c87ef615a6bf037655
DionKelmendi <108992835+DionKelmendi@users.noreply.github.com>
2024-01-19 17:06:41 +0100

    Updated RecipeTag Model

--- Flavorique Web App/Models/RecipeTag.cs
+++ Flavorique Web App/Models/RecipeTag.cs
@@ -2,6 +2,7 @@
 {
     public class RecipeTag
     {
+        public int Id { get; set; }
         public int RecipeId { get; set; }
         public Recipe Recipe { get; set; }
         public int TagId { get; set; }
